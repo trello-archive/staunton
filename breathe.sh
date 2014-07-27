9baf59d36a1c6a7ebad365ddee586c7adf4c5c7a
Hao Lian <me@haolian.org>
2014-07-27 13:08:59 -0400

    our first typo

--- breathe.sh
+++ breathe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ([[ -f server/breathe.sh ]] && echo "+ server alive") || echo "- no server"
-([[ -f client/client.sh ]] && echo "+ client alive") || echo "- no client"
-([[ -f observer/observer.sh ]] && echo "+ observer alive") || echo "- no server"
+([[ -f client/breathe.sh ]] && echo "+ client alive") || echo "- no client"
+([[ -f observer/breathe.sh ]] && echo "+ observer alive") || echo "- no server"
