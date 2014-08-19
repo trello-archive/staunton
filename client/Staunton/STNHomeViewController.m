3839a02f6eea0c9eacda943f9ec5fd4036bbab6c
Hao Lian <me@haolian.org>
2014-08-19 08:50:12 -0400

    Chess board view and controller

--- client/Staunton/STNHomeViewController.m
+++ client/Staunton/STNHomeViewController.m
@@ -1,6 +1,9 @@
 #import "STNHomeViewController.h"
+#import "STNChessBoardViewController.h"
 
 @interface STNHomeViewController ()
+
+@property (strong, nonatomic) STNChessBoardViewController *boardController;
 
 @end
 
@@ -8,13 +11,17 @@
 
 - (void )loadView {
     UIView *view = [UIView new];
-    view.layer.borderColor = [UIColor redColor].CGColor;
-    view.layer.borderWidth = 1;
     self.view = view;
 }
 
 - (void)viewDidLoad {
     self.view.frame = [[UIScreen mainScreen] applicationFrame];
+    self.boardController = [[STNChessBoardViewController alloc] init];
+
+    CGFloat side = MIN(self.view.frameSizeHeight, self.view.frameSizeWidth);
+    self.boardController.view.frame = CGRectMake(0, 0, side, side);
+    [self.view addSubview:self.boardController.view];
+    [self.boardController didMoveToParentViewController:self];
 }
 
 @end
