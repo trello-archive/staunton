9902308205b965f85f0be31d762d6dc489ae655f
Hao Lian <me@haolian.org>
2014-08-19 08:05:45 -0400

    breathing: client

--- breathe.sh
+++ breathe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
+set -e
 ([[ -f server/breathe.sh ]] && sh server/breathe.sh && echo "+ server alive") || echo "- no server"
 ([[ -f client/breathe.sh ]] && sh client/breathe.sh && echo "+ client alive") || echo "- no client"
-([[ -f observer/breathe.sh ]] && sh observer/breathe.sh && echo "+ observer alive") || echo "- no server"
