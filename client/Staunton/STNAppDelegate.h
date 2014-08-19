e5cab4cc7c20cc6dd020fdc4fd6965c2ceed1774
Hao Lian <me@haolian.org>
2014-08-19 08:13:35 -0400

    XCode: create empty application

---
+++ client/Staunton/STNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STNAppDelegate.h
+//  Staunton
+//
+//  Created by hao on 8/19/14.
+//  Copyright (c) 2014 trello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ client/Staunton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Staunton
+//
+//  Created by hao on 8/19/14.
+//  Copyright (c) 2014 trello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STNAppDelegate class]));
+    }
+}
