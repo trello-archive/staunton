20f60dbe67ac0ee26977ae4be827a10412ec8596
Ian Henry <ianthehenry@gmail.com>
2014-08-24 17:43:07 -0400

    added a randfloat() helper

---
+++ client/Staunton/STNHelpers.m
@@ -0,0 +1,13 @@
+//
+//  STNHelpers.m
+//  Staunton
+//
+//  Created by Ian Henry on 8/24/14.
+//  Copyright (c) 2014 trello. All rights reserved.
+//
+
+#import "STNHelpers.h"
+
+CGFloat randfloat() {
+    return (CGFloat)arc4random() / 0x100000000;
+}
