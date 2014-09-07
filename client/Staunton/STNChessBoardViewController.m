6d7541bf5fc45de2eede8f74200f2e4e8ce4d10c
Ian Henry <ianthehenry@gmail.com>
2014-09-07 19:28:51 -0400

    nicer drag handling: signal of signals, instead of weird hybrid signal + state observation

--- client/Staunton/STNChessBoardViewController.m
+++ client/Staunton/STNChessBoardViewController.m
@@ -5,17 +5,20 @@
 @interface STNChessBoardViewController ()
 
 @property (strong, nonatomic) RACSignal *diffSignal;
+@property (strong, nonatomic) UIView *myView;
+@property (strong, nonatomic) UIView *kingView;
 
 @end
 
 static UIImageView *makeGravatarView(CGFloat size) {
     UIImageView *view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size, size)];
-    UIBezierPath *shadowPath = [UIBezierPath bezierPathWithRect:view.bounds];
+    view.layer.cornerRadius = 0;
     view.layer.masksToBounds = NO;
     view.layer.shadowColor = [UIColor blackColor].CGColor;
-    view.layer.shadowOffset = CGSizeMake(2, 2);
+    view.layer.shadowOffset = CGSizeZero;
     view.layer.shadowOpacity = 0.5;
-    view.layer.shadowPath = shadowPath.CGPath;
+    view.layer.shadowRadius = 2;
+    view.layer.shadowPath = [UIBezierPath bezierPathWithRoundedRect:view.bounds cornerRadius:view.layer.cornerRadius].CGPath;
     return view;
 }
 
@@ -30,6 +33,71 @@ static UIImageView *makeGravatarView(CGFloat size) {
 
 - (void)loadView {
     self.view = [[STNChessBoardView alloc] init];
+}
+
+- (CGFloat)gravatarSize {
+    return self.view.bounds.size.width * 0.06;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    [self prepareMyView];
+    [self prepareKingView];
+
+    @weakify(self);
+    [[self.diffSignal groupBy:^(STNDiff *diff) {
+        return diff.email;
+    }] subscribeNext:^(RACGroupedSignal *perUserDiffs) {
+        @strongify(self);
+        [self handleDiffs:perUserDiffs forEmail:(NSString *)perUserDiffs.key];
+    }];
+}
+
+- (void)prepareKingView {
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.gravatarSize, self.gravatarSize)];
+    label.text = @"♚";
+    label.font = [UIFont systemFontOfSize:self.gravatarSize];
+    label.backgroundColor = [UIColor redColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
+    self.kingView = label;
+}
+
+- (RACSignal *)isDraggingSignal:(RACSignal *)dragSignal {
+    RACSubject *isDragging = [RACReplaySubject replaySubjectWithCapacity:1];
+    [isDragging sendNext:@NO];
+    [dragSignal subscribeNext:^(RACSignal *inner) {
+        [isDragging sendNext:@YES];
+        [inner subscribeCompleted:^{
+            [isDragging sendNext:@NO];
+        }];
+    }];
+    return isDragging;
+}
+
+- (void)prepareMyView {
+    UIImageView *gravatarView = makeGravatarView(self.gravatarSize);
+
+    UILongPressGestureRecognizer *recognizer = [self addRecognizer:gravatarView];
+    RACSignal *dragSignal = [self signalForRecognizer:recognizer];
+    RACSignal *isDragging = [self isDraggingSignal:dragSignal];
+
+    [gravatarView setImageWithGravatarEmailAddress:@"foo@bar.com"];
+
+    gravatarView.layer.borderColor = UIColor.whiteColor.CGColor;
+
+    RAC(gravatarView.layer, shadowOffset) = [RACSignal if:isDragging
+                                             then:[RACSignal return:[NSValue valueWithCGSize:CGSizeMake(2, 2)]]
+                                             else:[RACSignal return:[NSValue valueWithCGSize:CGSizeMake(0, 0)]]];
+    RAC(gravatarView.layer, shadowRadius) = [RACSignal if:isDragging
+                                             then:[RACSignal return:@5]
+                                             else:[RACSignal return:@1]];
+
+    RAC(gravatarView, center) = [dragSignal switchToLatest];
+
+    [self.view addSubview:gravatarView];
+    self.myView = gravatarView;
 }
 
 - (RACSignal *)centerForPosition:(RACSignal *)positionSignal {
@@ -51,15 +119,28 @@ static UIImageView *makeGravatarView(CGFloat size) {
     return recognizer;
 }
 
-- (RACSignal *)centerSignalFromRecognizer:(UILongPressGestureRecognizer *)recognizer {
-    RACSubject *centerSubject = [RACSubject subject];
+// Here's how I chose to model the drag and drop:
+//
+// The recognizer has a signal of "drags." This isn't a position!
+// Every time a touch begins, it sends a new signal with positions for the touch.
+// Every time a touch ends, that inner signal completes.
+// So dragging is a *signal of signals*.
+// This is more useful than just having a signal of positions, as
+// it lets us track when each drag event starts and stops.
+- (RACSignal *)signalForRecognizer:(UILongPressGestureRecognizer *)recognizer {
+    RACSubject *dragSubject = [RACSubject subject];
+
+    __block RACSubject *centerSubject;
     __block CGPoint initialPosition;
+
     [recognizer.rac_gestureSignal subscribeNext:^(UILongPressGestureRecognizer *recognizer) {
         UIView *view = recognizer.view;
         switch (recognizer.state) {
             case UIGestureRecognizerStateBegan:
                 initialPosition = [recognizer locationInView:view];
-                break;
+                centerSubject = [RACSubject subject];
+                [dragSubject sendNext:centerSubject];
+                // (deliberately falling through)
             case UIGestureRecognizerStateChanged: {
                 CGPoint topLeft = CGPointSubtract([recognizer locationInView:view.superview], initialPosition);
                 CGPoint center = CGPointAdd(topLeft, CGPointMake(CGRectGetMidX(view.bounds), CGRectGetMidY(view.bounds)));
@@ -67,17 +148,18 @@ static UIImageView *makeGravatarView(CGFloat size) {
                 break;
             }
             case UIGestureRecognizerStateEnded:
-                // blah blah, send update here
+                [centerSubject sendCompleted];
                 break;
             default: break;
         };
     }];
-    return centerSubject;
+
+    return dragSubject;
 }
 
 - (void)handleDiffs:(RACSignal *)diffs forEmail:(NSString *)email {
     __block UIImageView *gravatarView = nil;
-    
+
     RACSignal *insertions = [diffs filter:^BOOL(STNDiff *diff) {
         return diff.isInsert;
     }];
@@ -85,34 +167,23 @@ static UIImageView *makeGravatarView(CGFloat size) {
     RACSignal *removals = [diffs filter:^BOOL(STNDiff *diff) {
         return diff.isRemove;
     }];
-    
+
     [insertions subscribeNext:^(id x) {
         if (gravatarView != nil) {
             NSLog(@"dropping insertion of known email!");
             return;
         }
-        gravatarView = makeGravatarView(self.view.bounds.size.width * 0.06);
+        gravatarView = makeGravatarView(self.gravatarSize);
         [gravatarView setImageWithGravatarEmailAddress:email];
         [self.view addSubview:gravatarView];
 
         RACSignal *positionSignal = [[diffs takeUntil:removals] map:^(STNDiff *diff) {
             return [NSValue valueWithCGPoint:diff.point];
         }];
-        
-        UILongPressGestureRecognizer *recognizer = [self addRecognizer:gravatarView];
-        
-        RACSignal *isDragging = [RACObserve(recognizer, state) map:^id(NSNumber *stateNumber) {
-            return @(stateNumber.unsignedIntegerValue == UIGestureRecognizerStateBegan
-            || stateNumber.unsignedIntegerValue == UIGestureRecognizerStateChanged);
-        }];
-        
-        RAC(gravatarView, center) = [RACSignal if:isDragging
-                                     then:[self centerSignalFromRecognizer:recognizer]
-                                     else:[RACSignal defer:^{
-            return [[self centerForPosition:positionSignal] animated];
-        }]];
+
+        RAC(gravatarView, center) = [[self centerForPosition:positionSignal] animated];
     }];
-    
+
     [removals subscribeNext:^(id x) {
         if (gravatarView == nil) {
             NSLog(@"dropping removal of unknown email!");
@@ -120,18 +191,6 @@ static UIImageView *makeGravatarView(CGFloat size) {
         }
         [gravatarView removeFromSuperview];
         gravatarView = nil;
-    }];
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    
-    @weakify(self);
-    [[self.diffSignal groupBy:^(STNDiff *diff) {
-        return diff.email;
-    }] subscribeNext:^(RACGroupedSignal *perUserDiffs) {
-        @strongify(self);
-        [self handleDiffs:perUserDiffs forEmail:(NSString *)perUserDiffs.key];
     }];
 }
 
